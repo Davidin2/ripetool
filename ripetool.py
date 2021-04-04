3104160b86085cde9bbeecc40f957a34cdb000a9
Davidin <david.hernandezc@gmail.com>
2021-04-04 18:57:17 +0200

    Controlamos numero y cambos de prefijos

--- ripetool.py
+++ ripetool.py
@@ -8,6 +8,7 @@ import configparser
 AS=["AS1","AS2","AS3"]          #Lista de AS, tienen que ser 3
 ID=""                           #Para diferenciar si tienes varias instancias corriendo
 MAILS=["tu@email1","tu@email2"] #Direcciones de envío de mail
+PREFIX_DIFF=20            #diferencia de prefijos para mandar mail en valor absoluto
 
 def carga_config():
     global MAILS
@@ -23,6 +24,8 @@ def carga_config():
                 MAILS=config['default']['MAILS'].split(sep=',')
             if 'AS' in config['default']:
                 AS=config['default']['AS'].split(sep=',')
+            if 'PREFIX_DIFF' in config['default']:
+                PREFIX_DIFF=config['default']['PREFIX_DIFF']
 
 
     except (OSError, IOError) as e:
@@ -77,6 +80,7 @@ def carga_rangos(fichero):
 carga_config()
 HOST = "whois.ripe.net"
 COMANDO="-T route -xr --sources RIPE "
+COMANDO2="-i origin -T route -r --sources RIPE as"
 log=""
 fallo=0
 hora = datetime.now().replace(microsecond=0)
@@ -117,13 +121,84 @@ for rango in rangos:
         #envia_correo(texto, texto2)
         fallo+=1
     log=log+texto2 
+
+
+
+num_prefijos_antes=-1
+lista_prefijos_antes=[]
+try:
+    with open("num_prefijos.txt", "r") as fichero_prefijos:
+        for linea in fichero_prefijos:
+            lista_prefijos_antes.append(linea[:-1])
+        num_prefijos_antes=lista_prefijos_antes[0]
+except(OSError, IOError) as e:
+        print ("There is no files with last prefix sample")
+
+
+lista_prefijos_ahora=[]
+
+for ass in AS:
+
+    tn = telnetlib.Telnet(HOST,43)
+    comando=COMANDO2+ass
+    tn.write(comando.encode('ascii')+ b"\n")
+    result=tn.read_all().decode('ascii', 'ignore')
+    lista_result=result.splitlines()
+    for line in lista_result:
+        if line.find("route:")>-1:
+            ruta=line
+            ruta=ruta.replace(" ", "")
+            ruta=ruta.replace("route:", "")
+            lista_prefijos_ahora.append(ruta)
+
+
+
+
+num_prefijos_ahora=len(lista_prefijos_ahora)
+diferencia_de_rutas=num_prefijos_ahora-int(num_prefijos_antes)
+texto="<br><br>Route objects: " + str(num_prefijos_ahora)
+log=log+texto
+texto="<br><br>Last sample Route objects: " + str(num_prefijos_antes)
+log=log+texto
+
+with open("num_prefijos.txt", "w") as fichero_prefijos:
+    fichero_prefijos.write(str(num_prefijos_ahora)+"\n")
+    for prefijo in lista_prefijos_ahora:
+        fichero_prefijos.write(str(prefijo)+"\n")
+no_esta_ahora=[]
+no_estaba_antes=[]
+
+for prefijo_antes in lista_prefijos_antes:
+    if "/" in str(prefijo_antes):
+        if prefijo_antes not in lista_prefijos_ahora:
+            no_esta_ahora.append(prefijo_antes)
+for prefijo_ahora in lista_prefijos_ahora:
+    if "/" in str(prefijo_ahora):
+        if prefijo_ahora not in lista_prefijos_antes:
+            no_estaba_antes.append(prefijo_ahora)
+
+
+texto="<br><br>These prefixes were before and now they are not: " + str(no_esta_ahora)
+log=log+texto
+texto="<br><br>These prefixes are now and they were not before: " + str(no_estaba_antes)
+log=log+texto
+
 hora_fin = datetime.now().replace(microsecond=0)
 texto2="</table><BR>-------------End Date: " + str(hora_fin)
 log=log + texto2
 
 if (fallo>0):
     envia_correo(str(fallo)+ " RANGES WITH PROBLEMS in RIPE",log)
+if ((diferencia_de_rutas>int(PREFIX_DIFF)) or (diferencia_de_rutas<-int(PREFIX_DIFF))):
+    envia_correo("Sudden change of  " + str(diferencia_de_rutas) + " route objects in RIPE from the previous sample",log)
 if hora.hour==0:
     envia_correo("Daily report",log)
 print(log)
 
+logfile=open("lista_cambios.txt", "a")
+for prefijo in no_esta_ahora:
+    print(str(hora_fin)+" - "+ str(prefijo), file=logfile)
+for prefijo in no_estaba_antes:
+    print(str(hora_fin)+" + "+ str(prefijo), file=logfile)
+
+logfile.close()
